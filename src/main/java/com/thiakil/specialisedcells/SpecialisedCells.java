c6d2cbc68caaf26208ff48acbdbb2472a015b515
Thiakil <xander@thiakil.com>
2023-10-28 10:51:09 +0800

    dont register unused config

--- src/main/java/com/thiakil/specialisedcells/SpecialisedCells.java
+++ src/main/java/com/thiakil/specialisedcells/SpecialisedCells.java
@@ -56,7 +56,7 @@ public class SpecialisedCells
         MinecraftForge.EVENT_BUS.register(this);
 
         // Register our mod's ForgeConfigSpec so that Forge can create and load the config file for us
-        ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, Config.SPEC);
+        //ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, Config.SPEC);
     }
 
     private void commonSetup(final FMLCommonSetupEvent event)
